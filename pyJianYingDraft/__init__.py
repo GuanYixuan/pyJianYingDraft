fd838640cfd48a2b6ce47a2eff6560935a94c196
gary318 <gary318@126.com>
2024-07-21 10:20:37 +0800

    Forming package structure

---
+++ pyJianYingDraft/__init__.py
@@ -0,0 +1,4 @@
+from Jianying_json import Video_material, Track_segment, Timerange, Script_file
+
+from Jianying_json import Keyframe_property
+from animation_meta import Video_intro_type, Video_outro_type
