2406b797695ae96815728dae50bd43086e21b0e8
gary318 <gary318@126.com>
2024-07-21 22:21:07 +0800

    Migrate Video_material and Crop_settings to local_materials.py

--- pyJianYingDraft/__init__.py
+++ pyJianYingDraft/__init__.py
@@ -1,4 +1,5 @@
-from Jianying_json import Video_material, Track_segment, Timerange, Script_file
+from local_materials import Crop_settings, Video_material
+from Jianying_json import Track_segment, Timerange, Script_file
 
 from Jianying_json import Keyframe_property
 from animation_meta import Video_intro_type, Video_outro_type
