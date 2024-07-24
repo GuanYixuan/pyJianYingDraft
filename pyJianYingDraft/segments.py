41848162173df8d9a547dcc00abc08e072f5b283
gary318 <gary318@126.com>
2024-07-24 09:22:52 +0800

    Refactor Video_segment as a subclass of Base_segment

--- pyJianYingDraft/segments.py
+++ pyJianYingDraft/segments.py
@@ -214,12 +214,10 @@ class Base_segment:
             "enable_lut": True,
             "enable_smart_color_adjust": False,
             "group_id": "",
-            # "hdr_settings"
             "intensifies_audio": False,
             "is_placeholder": False,
             "is_tone_modify": False,
             "last_nonzero_volume": 1.0,
-            # "render_index": 0,
             "responsive_layout": {"enable": False, "horizontal_pos_layout": 0, "size_layout": 0, "vertical_pos_layout": 0, "target_follow": ""},
             "reverse": False,
             "template_id": "",
@@ -240,22 +238,9 @@ class Base_segment:
             "keyframe_refs": [], # 意义不明
         }
 
-class Video_segment:
+class Video_segment(Base_segment):
     """安放在轨道上的一个视频/图片片段"""
 
-    segment_id: str
-    """片段全局id, 自动生成"""
-    material_id: str
-    """使用的素材id"""
-    source_timerange: Timerange
-    """截取的素材片段的时间范围"""
-    target_timerange: Timerange
-    """片段在轨道上的时间范围"""
-
-    common_keyframes: List[Keyframe_list]
-    """各属性的关键帧列表"""
-    extra_material_refs: List[str]
-    """附加的素材id列表, 用于链接动画等"""
     clip_settings: Clip_settings
     """图像调节设置, 其效果可被关键帧覆盖"""
 
@@ -264,9 +249,8 @@ class Video_segment:
     animations_instance: Optional[Segment_animations]
     """动画实例, 可能为空"""
 
-    def __init__(self, material: Video_material,
-                 target_timerange: Timerange,
-                 source_timerange: Optional[Timerange] = None,
+    def __init__(self, material: Video_material, target_timerange: Timerange, *,
+                 source_timerange: Optional[Timerange] = None, speed: Optional[float] = None, volume: float = 1.0,
                  clip_settings: Optional[Clip_settings] = None):
         """利用给定的视频/图片素材构建一个轨道片段, 并指定其时间信息及图像调节设置
 
@@ -275,21 +259,24 @@ class Video_segment:
         Args:
             material (`Video_material`): 素材实例, 注意你仍然需要为其调用`Script_file.add_material`来将其添加到素材列表中
             target_timerange (`Timerange`): 片段在轨道上的目标时间范围
-            source_timerange (`Timerange`, optional): 截取的素材片段的时间范围, 默认从开头截取与`target_timerange`等长的一部分
+            source_timerange (`Timerange`, optional): 截取的素材片段的时间范围, 默认从开头根据`speed`截取与`target_timerange`等长的一部分
+            speed (`float`, optional): 播放速度, 默认为1.0. 此项与`source_timerange`同时指定时, 将覆盖`target_timerange`中的时长
+            volume (`float`, optional): 音量, 默认为1.0
             clip_settings (`Clip_settings`, optional): 图像调节设置, 默认不作任何变换
         """
-        self.segment_id = uuid.uuid4().hex
-        self.material_id = material.material_id
+        if source_timerange is not None and speed is not None:
+            target_timerange = Timerange(target_timerange.start, round(source_timerange.duration / speed))
+        elif source_timerange is not None and speed is None:
+            speed = source_timerange.duration / target_timerange.duration
+        else: # source_timerange is None
+            speed = speed if speed is not None else 1.0
+            source_timerange = Timerange(0, round(target_timerange.duration * speed))
+
+        super().__init__(material.material_id, source_timerange, target_timerange, speed, volume)
 
         self.clip_settings = clip_settings if clip_settings is not None else Clip_settings()
-
-        self.common_keyframes = []
-        self.extra_material_refs = []
         self.uniform_scale = True
         self.animations_instance = None
-
-        self.target_timerange = target_timerange
-        self.source_timerange = source_timerange if source_timerange is not None else Timerange(0, target_timerange.duration)
 
     def attach_animation(self, animation: Segment_animations) -> None:
         """将给定的动画链接到此片段, 你仍然需要调用`Script_file.add_animation`来将动画添加到素材列表中"""
@@ -344,46 +331,13 @@ class Video_segment:
         self.common_keyframes.append(kf_list)
 
     def export_json(self) -> Dict[str, Any]:
-        fields = {
-            # 定义一致字段的默认值
-            "caption_info": None,
-            "cartoon": False,
-            "enable_adjust": True,
-            "enable_color_correct_adjust": False,
-            "enable_color_curves": True,
-            "enable_color_match_adjust": False,
-            "enable_color_wheels": True,
-            "enable_lut": True,
-            "enable_smart_color_adjust": False,
-            "group_id": "",
-            "hdr_settings": {"intensity": 1.0, "mode": 1, "nits": 1000},
-            "intensifies_audio": False,
-            "is_placeholder": False,
-            "is_tone_modify": False,
-            "last_nonzero_volume": 1.0,
-            "render_index": 0,
-            "responsive_layout": {"enable": False, "horizontal_pos_layout": 0, "size_layout": 0, "vertical_pos_layout": 0, "target_follow": ""},
-            "reverse": False,
-            "speed": 1.0,
-            "template_id": "",
-            "template_scene": "default",
-            "track_attribute": 0,
-            "track_render_index": 0,
-            "visible": True,
-            "volume": 1.0,
-            # 写入自定义字段
-            "id": self.segment_id,
-            "material_id": self.material_id,
-            "common_keyframes": [kf_list.export_json() for kf_list in self.common_keyframes],
-            "extra_material_refs": self.extra_material_refs,
-            "keyframe_refs": [], # 意义不明
-            "source_timerange": self.source_timerange.export_json(),
-            "target_timerange": self.target_timerange.export_json(),
+        json_dict = super().export_json()
+        json_dict.update({
             "clip": self.clip_settings.export_json(),
+            "hdr_settings": {"intensity": 1.0, "mode": 1, "nits": 1000},
             "uniform_scale": {"on": self.uniform_scale, "value": 1.0},
-        }
-
-        return fields
+        })
+        return json_dict
 
 class Audio_fade:
     """音频淡入淡出效果"""
