ac7ffcb8804a76132ff5759edcea1ac4b5879173
gary318 <gary318@126.com>
2024-10-16 10:02:07 +0800

    Fetch metadata for text animations

--- pyJianYingDraft/metadata/__init__.py
+++ pyJianYingDraft/metadata/__init__.py
@@ -6,6 +6,7 @@ from .mask_meta import Mask_type, Mask_meta
 from .filter_meta import Filter_type
 from .transition_meta import Transition_type
 from .animation_meta import Intro_type, Outro_type, Group_animation_type
+from .animation_meta import Text_intro, Text_outro, Text_loop_anim
 from .audio_effect_meta import Audio_scene_effect_type, Tone_effect_type, Speech_to_song_type
 from .video_effect_meta import Video_scene_effect_type, Video_character_effect_type
 
@@ -19,6 +20,9 @@ __all__ = [
     "Intro_type",
     "Outro_type",
     "Group_animation_type",
+    "Text_intro",
+    "Text_outro",
+    "Text_loop_anim",
     "Audio_scene_effect_type",
     "Tone_effect_type",
     "Speech_to_song_type",
