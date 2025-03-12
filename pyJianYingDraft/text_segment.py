975b4b63f5e0aaee4cbae0651db8d549aa66e774
gary318 <gary318@126.com>
2025-03-12 23:46:48 +0800

    Feat: Implement data class TextBubble

--- pyJianYingDraft/text_segment.py
+++ pyJianYingDraft/text_segment.py
@@ -94,6 +94,31 @@ class Text_border:
             "width": self.width
         }
 
+class TextBubble:
+    """文本气泡素材, 与滤镜素材本质上一致"""
+
+    global_id: str
+    """花字全局id, 由程序自动生成"""
+
+    effect_id: str
+    resource_id: str
+
+    def __init__(self, effect_id: str, resource_id: str):
+        self.global_id = uuid.uuid4().hex
+        self.effect_id = effect_id
+        self.resource_id = resource_id
+
+    def export_json(self) -> Dict[str, Any]:
+        return {
+            "apply_target_type": 0,
+            "effect_id": self.effect_id,
+            "id": self.global_id,
+            "resource_id": self.resource_id,
+            "type": "text_shape",
+            "value": 1.0,
+            # 不导出path和request_id
+        }
+
 class Text_segment(Visual_segment):
     """文本片段类, 目前仅支持设置基本的字体样式"""
 
