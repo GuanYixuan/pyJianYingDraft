47401fe63068d7a2a8154d909eeae01e22d08d65
gary318 <gary318@126.com>
2025-03-22 23:35:31 +0800

    Chore: Add setup.py for package configuration

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyJianYingDraft",
+    version="0.1.0",
+    author="gary318",
+    description="轻量、灵活、易上手的Python剪映草稿生成及导出工具，构建全自动化视频剪辑/混剪流水线",
+    long_description=open("pypi_readme.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/GuanYixuan/pyJianYingDraft",
+    packages=find_packages(),
+    package_data={
+        'pyJianYingDraft': ['draft_content_template.json']
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Operating System :: Microsoft :: Windows",
+        "Topic :: Multimedia :: Video"
+    ],
+    python_requires='>=3.8',
+    install_requires=[
+        "pymediainfo",
+        "imageio",
+        "uiautomation>=2"
+    ],
+)
