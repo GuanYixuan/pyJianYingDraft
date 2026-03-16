5015c4d421ab39922d8b95dd07040f7786287e3d
gary318 <gary318@126.com>
2026-03-16 10:37:16 +0800

    Chore: Bump version to 0.2.6

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name="pyJianYingDraft",
-    version="0.2.5",
+    name="pyjianyingdraft",
+    version="0.2.6",
     author="gary318",
     description="轻量、灵活、易上手的Python剪映草稿生成及导出工具，构建全自动化视频剪辑/混剪流水线",
     long_description=open("pypi_readme.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/GuanYixuan/pyJianYingDraft",
-    packages=["pyJianYingDraft"],
+    packages=find_packages(exclude=["tools", "tools.*", "ignored", "ignored.*"]),
     package_data={
-        'pyJianYingDraft.assets': ['*.json']
+        'pyJianYingDraft': ['assets/*.json']
     },
     classifiers=[
         "Programming Language :: Python :: 3",
